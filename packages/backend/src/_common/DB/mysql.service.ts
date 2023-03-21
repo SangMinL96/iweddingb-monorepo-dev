5900af92dcade044e64540eb4d051696c5e898f2
sangmin.lee <hwon3794@gmail.com>
2023-03-21 15:23:40 +0900

    mysql2/promise utf8

--- packages/backend/src/_common/DB/mysql.service.ts
+++ packages/backend/src/_common/DB/mysql.service.ts
@@ -6,12 +6,22 @@ import {
   PoolConnection,
   ResultSetHeader,
 } from 'mysql2/promise';
+import { async } from 'rxjs';
 
 @Injectable()
 export class MysqlService {
   constructor() {
     //
   }
+
+  async utf8(conn: PoolConnection): Promise<any> {
+    await conn.query(`set session character_set_connection=utf8`);
+    await conn.query(`set session character_set_client=utf8`);
+    await conn.query(`set session character_set_results=utf8`);
+    await conn.query(`set collation_connection=utf8_general_ci`);
+    await conn.query(`set names utf8`);
+  }
+
   async conn(): Promise<PoolConnection> {
     const dbConfig = {
       host: process.env.DATABASE_HOST,
@@ -26,15 +36,16 @@ export class MysqlService {
     pool = await createPool({
       ...dbConfig,
       namedPlaceholders: true,
+      charset: 'utf8',
     });
     return pool.getConnection();
   }
   async getQuery<T>(sql: string, params?: any): Promise<T> {
     const conn = await this.conn();
-
     try {
       if (conn) {
         await conn.beginTransaction();
+        await this.utf8(conn);
         const [row] = await conn.query<ResultSetHeader>(sql, params);
         return row as T;
       }
@@ -50,6 +61,7 @@ export class MysqlService {
     try {
       if (conn) {
         await conn.beginTransaction();
+        await this.utf8(conn);
         const [row] = await conn.query<ResultSetHeader>(sql, params);
         if (row) {
           return { result: 'success' };
@@ -67,6 +79,7 @@ export class MysqlService {
     try {
       if (conn) {
         await conn.beginTransaction();
+        await this.utf8(conn);
         const [row] = await conn.query<ResultSetHeader>(sql, params);
         return { insertId: row.insertId } as T;
       }
