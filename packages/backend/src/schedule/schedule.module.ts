399c3aabcbbed2b545582b0e52854a89a162d671
sangmin.lee <hwon3794@gmail.com>
2023-03-31 16:37:38 +0900

    .

--- packages/backend/src/schedule/schedule.module.ts
+++ packages/backend/src/schedule/schedule.module.ts
@@ -1,8 +1,8 @@
 import { MysqlService } from '@common/DB/mysql.service';
 import { Module } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
-import { ScheduleController } from './Schedule.controller';
-import { ScheduleService } from './Schedule.service';
+import { ScheduleController } from './schedule.controller';
+import { ScheduleService } from './schedule.service';
 
 @Module({
   imports: [],
