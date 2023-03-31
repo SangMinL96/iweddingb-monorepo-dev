aafbc3d4fae7a38c4799f0cb3f031c961811a2be
sangmin.lee <hwon3794@gmail.com>
2023-03-31 17:13:32 +0900

    nestjs 모듈 설치

--- packages/backend/src/app/app.module.ts
+++ packages/backend/src/app/app.module.ts
@@ -3,20 +3,20 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'src/auth/auth.module';
 import { EnterpriseModule } from 'src/enterprise/enterprise.module';
 import { LoginModule } from 'src/login/login.module';
-import { ScheduleModule } from 'src/schedule/schedule.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from 'src/schedule/schedule.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: dotEnv(),
-    }),
     LoginModule,
     AuthModule,
     EnterpriseModule,
     ScheduleModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: dotEnv(),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- packages/backend/src/schedule/schedule.module.ts
+++ packages/backend/src/schedule/schedule.module.ts
@@ -1,4 +1,5 @@
 import { MysqlService } from '@common/DB/mysql.service';
+import { JwtStrategy } from '@common/jwt/jwtStrategy';
 import { Module } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { ScheduleController } from './schedule.controller';
@@ -7,6 +8,6 @@ import { ScheduleService } from './schedule.service';
 @Module({
   imports: [],
   controllers: [ScheduleController],
-  providers: [ScheduleService, MysqlService, JwtService],
+  providers: [ScheduleService, MysqlService, JwtService, JwtStrategy],
 })
 export class ScheduleModule {}
