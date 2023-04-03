cdca153c6b58f51de93b1d0658b5500d4a9fb077
sangmin.lee <hwon3794@gmail.com>
2023-04-03 14:25:13 +0900

    .

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true, credentials: true });
+  const CORS_URL = process.env.CORS_URL.split("::");
+  app.enableCors({ origin: CORS_URL, credentials: true });
   const port = process.env.PORT;
   await app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/api`);
